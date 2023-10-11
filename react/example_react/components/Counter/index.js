7aaf8e419e3902a0565a103e703a22b4714e61ca
kenley.gaitan <kenley.gaitan@smbssolutions.com>
2023-10-11 14:30:15 -0600

    se valido el counter para q no acepte numeros menores a 1

--- react/example_react/components/Counter/index.js
+++ react/example_react/components/Counter/index.js
@@ -3,16 +3,25 @@ import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
 const Counter = () => {
+  const handleCounter = (input) => {
+    console.log(input.target.value);
+  };
+
   return (
-    <TextField
-      id="filled-number"
-      label="Number"
-      type="number"
-      InputLabelProps={{
-        shrink: true,
-      }}
-      variant="filled"
-    />
+    <form>
+      <TextField
+        id="filled-number"
+        label="Number"
+        type="number"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        variant="filled"
+        inputProps={{ min: 1, max: 100 }}
+        onChange={handleCounter}
+      />
+      <button type="submit">submit</button>
+    </form>
   );
 };
 
